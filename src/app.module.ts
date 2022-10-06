b6670aacc98f7e673ba75bff67d0d5297f02a97a
Cinthia <votacinthia@gmail.com>
2022-10-06 10:59:10 -0300

    Elimino carpeta juego - bases html editar - crear nproy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { JuegoController } from './juego/juego.controller';
-//import { JuegoService } from './juego/juego.service';
-//import { JuegoModule } from './juego/juego.module';
-import { CalculadoraController } from './calculadora/calculadora.controller';
-import { CalculadoraService } from './calculadora/calculadora.service';
-
-import { VehiculosController } from './vehiculos/vehiculos.controller';
-import { VehiculosService } from './vehiculos/vehiculos.service';
+// import { CalculadoraController } from './calculadora/calculadora.controller';
+// import { CalculadoraService } from './calculadora/calculadora.service';
+// import { VehiculosController } from './vehiculos/vehiculos.controller';
+// import { VehiculosService } from './vehiculos/vehiculos.service';
 
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'app') }),
-    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'juego') }),
-    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'calculadora') }),
-    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'vehiculo') }),
+    // ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'calculadora') }),
+    // ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'vehiculo') }),
   ],
-  controllers: [AppController, JuegoController, CalculadoraController, VehiculosController],
-  providers: [AppService, /* JuegoService, */ CalculadoraService, VehiculosService],
+  controllers: [AppController, /* CalculadoraController, VehiculosController */],
+  providers: [AppService, /* CalculadoraService, VehiculosService */],
 })
 export class AppModule {}
